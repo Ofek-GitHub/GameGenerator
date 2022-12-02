73742da91b926615bd4c16dfff488d491524aa81
Ofek-GitHub <ofeksimantov53@gmail.com>
2022-12-02 12:51:17 +0200

    Changed lines to start working on project

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print("Hello world!")
+def get_number_of_teams():
+    while True:
+        num_of_teams = input("Please enter the number of teams in the tournaments:")
+        if num_of_teams > 2 :
+            print("The amount of teams entered is invalid, please try again!")
