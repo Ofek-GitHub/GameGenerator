ac3e18a0c628c7320ca2617a446cfd02f3d200dc
Ofek-GitHub <ofeksimantov53@gmail.com>
2022-12-04 18:29:47 +0200

    finished this simple project

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 def get_number_of_teams():
     while True:
-        num_of_teams = input("Please enter the number of teams in the tournaments:")
-        if num_of_teams < 2 :
-            print("The amount of teams entered is invalid, please try again!")
-            break
-        return num_of_teams
+        num_teams = int(input("Enter the number of teams in the tournament: "))
 
+        if num_teams >= 2 and num_teams % 2 == 0:
+            break
+
+        print("The minimum number of teams is 2 and it has to be an even number, try again.")
+
+    return num_teams
 
 def get_teams_names(num_of_teams):
     team_names = []
@@ -23,16 +25,42 @@ def get_teams_names(num_of_teams):
     return team_names
 
 def get_number_of_games_played(num_teams):
-    while True:
-        games_played = int(
-            input("Enter the number of games played by each team: "))
+        games_played = num_of_teams -1
+        return games_played
+        
 
-        if games_played >= num_teams - 1:
-            break
+def get_num_of_wins(team_names, games_played):
+    team_wins = []
+    for team in team_names:
+        while True:
+            wins = int(input("Please enter the amount of wins " + team +" team has had:"))
+            if wins > games_played:
+                print("The amount of wins is bigger then games played, please try again.")
+            elif wins < 0 :
+                print("This number is invalid, team cant have a negetive amount of wins! try again!")
+            else:
+                break
+        team_wins.append((team,wins))
+    return team_wins
 
-        print("Invalid number of games. Each team plays each other at least once in the regular season, try again.")
+def get_second_item(teams):
+    return teams[1]
 
-    return games_played        
+num_of_teams = get_number_of_teams()
+names = get_teams_names(num_of_teams)
+games_played = get_number_of_games_played(num_of_teams)
+wins = get_num_of_wins(names,games_played)
+print(wins)
+sorted_teams = sorted(wins,key = get_second_item)
+print(sorted_teams)
+
+print("Generating games for the first round...")
+
+for i in range(int(num_of_teams/2)):
+    Away = sorted_teams[i][0]
+    Home = sorted_teams[-1][0]
+    sorted_teams.pop() 
+    print(f"Home: {Home} VS Away: {Away}")
 
             
 
